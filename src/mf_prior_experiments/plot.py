ae10d39b86e94d666cb933aede3c10548c12d42e
neeratyoy <neeratyoy@gmail.com>
2022-09-22 19:56:04 +0200

    Fixing to absolute path on import

--- src/mf_prior_experiments/plot.py
+++ src/mf_prior_experiments/plot.py
@@ -8,9 +8,9 @@ import numpy as np
 import seaborn as sns
 from attrdict import AttrDict
 
-from .configs.plotting.read_results import get_seed_info, load_yaml
-from .configs.plotting.styles import X_LABEL, Y_LABEL
-from .configs.plotting.utils import plot_incumbent, save_fig, set_general_plot_style
+from mf_prior_experiments.configs.plotting.read_results import get_seed_info, load_yaml
+from mf_prior_experiments.configs.plotting.styles import X_LABEL, Y_LABEL
+from mf_prior_experiments.configs.plotting.utils import plot_incumbent, save_fig, set_general_plot_style
 
 benchmark_configs_path = os.path.join(os.path.dirname(__file__), "configs/benchmark/")
 
