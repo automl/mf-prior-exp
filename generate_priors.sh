0a82e37336599f51acd7c21ec8ab41498903b453
eddiebergman <eddiebergmanhs@gmail.com>
2023-01-13 15:55:09 +0100

    fix: Make same seed as generate

--- generate_priors.sh
+++ generate_priors.sh
@@ -16,6 +16,6 @@ prior_directory="$(pwd)/src/mf-prior-bench/priors"
 echo "Running:"
 python -m mfpbench generate-priors \
     --to "${prior_directory}" \
-    --seed 8080 \
+    --seed 1330777 \
     --nsamples 100 \
     --clean
