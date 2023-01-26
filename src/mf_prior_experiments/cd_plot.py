fix(plot): Parallel runs x scaling

src/mf_prior_experiments/cd_plot.py
@@ -15,7 +15,7 @@ from joblib import Parallel, delayed, parallel_backend
 from path import Path
 from scipy.stats import friedmanchisquare, wilcoxon
 
-from .configs.plotting.read_results import get_seed_info
+from .configs.plotting.read_results import get_seed_info, SINGLE_FIDELITY_ALGORITHMS
 from .configs.plotting.utils import (
     ALGORITHMS,
     get_max_fidelity,
@@ -506,7 +506,16 @@ def wilcoxon_holm(df_perf, performance_metric_column_name="roc_auc", alpha=0.05)
     return p_values, average_ranks, max_nb_datasets
 
 
-def _process_seed(_path, seed, algorithm, key_to_extract, cost_as_runtime, results):
+def _process_seed(
+    _path,
+    seed,
+    algorithm,
+    key_to_extract,
+    cost_as_runtime,
+    results,
+    n_workers,
+    parallel_sleep_decrement,  # Needed for parallel setups
+):
     print(
         f"[{time.strftime('%H:%M:%S', time.localtime())}] "
         f"[-] [{algorithm}] Processing seed {seed}..."
@@ -514,7 +523,12 @@ def _process_seed(_path, seed, algorithm, key_to_extract, cost_as_runtime, resul
 
     # `algorithm` is passed to calculate continuation costs
     losses, infos, max_cost = get_seed_info(
-        _path, seed, algorithm=algorithm, cost_as_runtime=cost_as_runtime
+        _path,
+        seed,
+        algorithm=algorithm,
+        cost_as_runtime=cost_as_runtime,
+        n_workers=n_workers,
+        parallel_sleep_decrement=parallel_sleep_decrement,
     )
     incumbent = np.minimum.accumulate(losses)
     cost = [i[key_to_extract] for i in infos]
@@ -601,6 +615,8 @@ def plot(args):
                                 KEY_TO_EXTRACT,
                                 args.cost_as_runtime,
                                 results,
+                                args.n_workers,
+                                args.parallel_sleep_decrement,
                             )
                             for seed in seeds
                         )
@@ -615,6 +631,8 @@ def plot(args):
                             KEY_TO_EXTRACT,
                             args.cost_as_runtime,
                             results,
+                            args.n_workers,
+                            args.parallel_sleep_decrement,
                         )
                         for seed in seeds
                     ]
@@ -626,10 +644,31 @@ def plot(args):
                 if args.n_workers > 1 and max_cost is None:
                     max_cost = get_max_fidelity(benchmark_name=benchmark)
 
-                df = interpolate_time(incumbents, costs, scale_x=max_cost)
+                df = interpolate_time(
+                    incumbents,
+                    costs,
+                    scale_x=max_cost,
+                    parallel_evaluations=(args.n_workers > 1),
+                    # Single fidelity runners may incur some slight machine overhead but
+                    # in practice and theory, this would be non-existent or neglible, they
+                    # will always have a unit cost of an epoch
+                    rounded_integer_costs_for_x_range=(algorithm in SINGLE_FIDELITY_ALGORITHMS)
+                )
 
                 if budget is not None:
+                    pre_index = df.index
+                    if algorithm in SINGLE_FIDELITY_ALGORITHMS:
+                        df.index = df.index.astype(int)
+
                     df = df.query(f"index <= {budget}")
+                    if len(df) == 0:
+                        raise ValueError(
+                            f"No values in dataframe index were below the budget: {budget}."
+                            f"\nalgorithm: {algorithm} | benchmark: {benchmark}"
+                            f"\nThe costs recorded in dataframe index are {pre_index}"
+                        )
+
+                # Take mean across seeds
                 final_mean = df.mean(axis=1).values[-1]
 
                 df_perf.loc[len(df_perf)] = [benchmark, final_mean, ALGORITHMS[algorithm]]

src/mf_prior_experiments/configs/plotting/utils.py
@@ -7,7 +7,7 @@ import pandas as pd
 from path import Path
 from scipy import stats
 
-from .styles import ALGORITHMS, COLOR_MARKER_DICT, DATASETS
+from .styles import ALGORITHMS, COLOR_MARKER_DICT, DATASETS, BENCHMARK_COLORS
 
 
 def get_max_fidelity(benchmark_name):
@@ -123,6 +123,12 @@ def get_parser():
         default="multiprocessing",
         help="which backend use for parallel",
     )
+    parser.add_argument(
+        "--parallel_sleep_decrement",
+        default=10,
+        type=int,
+        help="The sleep timer used during parallel runs"
+    )
 
     return parser
 
@@ -169,12 +175,22 @@ def save_fig(fig, filename, output_dir, extension="pdf", dpi: int = 100):
     print(f'Saved to "{output_dir}/{filename}.{extension}"')
 
 
-def interpolate_time(incumbents, costs, x_range=None, scale_x=None):
-    df_dict = {}
+def interpolate_time(
+    incumbents,
+    costs,
+    x_range=None,
+    scale_x=None,
+    parallel_evaluations: bool = False,
+    rounded_integer_costs_for_x_range: bool = False,
+):
+    df_dict = {
+        f"seed{i}": pd.Series(seed_incs, index=seed_costs)
+        for i, (seed_incs, seed_costs) in enumerate(zip(incumbents, costs))
+    }
+    if not parallel_evaluations:
+        for k, series in df_dict.items():
+            series.index = np.cumsum(series.index)
 
-    for i, _ in enumerate(incumbents):
-        _seed_info = pd.Series(incumbents[i], index=np.cumsum(costs[i]))
-        df_dict[f"seed{i}"] = _seed_info
     df = pd.DataFrame.from_dict(df_dict)
 
     # important step to plot func evals on x-axis
@@ -182,18 +198,28 @@ def interpolate_time(incumbents, costs, x_range=None, scale_x=None):
 
     if x_range is not None:
         min_b, max_b = x_range
+
         new_entry = {c: np.nan for c in df.columns}
         _df = pd.DataFrame.from_dict(new_entry, orient="index").T
         _df.index = [min_b]
         df = pd.concat((df, _df)).sort_index()
+
         new_entry = {c: np.nan for c in df.columns}
         _df = pd.DataFrame.from_dict(new_entry, orient="index").T
         _df.index = [max_b]
         df = pd.concat((df, _df)).sort_index()
 
     df = df.fillna(method="backfill", axis=0).fillna(method="ffill", axis=0)
+
     if x_range is not None:
-        df = df.query(f"{x_range[0]} <= index <= {x_range[1]}")
+        lower, upper = x_range
+
+        if rounded_integer_costs_for_x_range:
+            _index = df.index.astype(int)
+        else:
+            _index = df.index
+
+        df = df[(lower <= df.index) & (df.index <= upper)]
 
     return df
 
@@ -293,7 +319,7 @@ def plot_incumbent(
     # ax.set_ylim(auto=True)
 
     if title is not None:
-        ax.set_title(DATASETS[title], fontsize=20)
+        ax.set_title(DATASETS[title], fontsize=20, color=BENCHMARK_COLORS[title])
     if xlabel is not None:
         ax.set_xlabel(xlabel, fontsize=18, color=(0, 0, 0, 0.69))
     if ylabel is not None:

src/mf_prior_experiments/final_table.py
@@ -11,13 +11,22 @@ from joblib import Parallel, delayed, parallel_backend
 from path import Path
 from scipy import stats
 
-from .configs.plotting.read_results import get_seed_info
+from .configs.plotting.read_results import get_seed_info, SINGLE_FIDELITY_ALGORITHMS
 from .configs.plotting.utils import interpolate_time
 
 benchmark_configs_path = os.path.join(os.path.dirname(__file__), "configs/benchmark/")
 
 
-def _process_seed(_path, seed, algorithm, key_to_extract, cost_as_runtime, results):
+def _process_seed(
+    _path,
+    seed,
+    algorithm,
+    key_to_extract,
+    cost_as_runtime,
+    results,
+    n_workers,
+    parallel_sleep_decrement,
+):
     print(
         f"[{time.strftime('%H:%M:%S', time.localtime())}] "
         f"[-] [{algorithm}] Processing seed {seed}..."
@@ -25,7 +34,12 @@ def _process_seed(_path, seed, algorithm, key_to_extract, cost_as_runtime, resul
 
     # `algorithm` is passed to calculate continuation costs
     losses, infos, max_cost = get_seed_info(
-        _path, seed, algorithm=algorithm, cost_as_runtime=cost_as_runtime
+        _path,
+        seed,
+        algorithm=algorithm,
+        cost_as_runtime=cost_as_runtime,
+        n_workers=n_workers,
+        parallel_sleep_decrement=parallel_sleep_decrement
     )
     incumbent = np.minimum.accumulate(losses)
     cost = [i[key_to_extract] for i in infos]
@@ -90,6 +104,8 @@ def plot(args):
                             KEY_TO_EXTRACT,
                             args.cost_as_runtime,
                             results,
+                            args.n_workers,
+                            args.parallel_sleep_decrement,
                         )
                         for seed in seeds
                     )
@@ -105,6 +121,7 @@ def plot(args):
                         KEY_TO_EXTRACT,
                         args.cost_as_runtime,
                         results,
+                        args.parallel_sleep_decrement,
                     )
                     for seed in seeds
                 ]
@@ -113,7 +130,13 @@ def plot(args):
             costs = np.array(results["costs"][:])
             max_cost = None if args.cost_as_runtime else max(results["max_costs"][:])
 
-            df = interpolate_time(incumbents, costs, scale_x=max_cost)
+            df = interpolate_time(
+                incumbents,
+                costs,
+                scale_x=max_cost,
+                parallel_evaluation=(args.n_workers > 1),
+                rounded_integer_costs_for_x_range=(algorithm in SINGLE_FIDELITY_ALGORITHMS)
+            )
 
             if args.budget is not None:
                 df = df.query(f"index <= {args.budget}")

src/mf_prior_experiments/plot.py
@@ -10,7 +10,7 @@ import seaborn as sns
 from attrdict import AttrDict
 from joblib import Parallel, delayed, parallel_backend
 
-from .configs.plotting.read_results import get_seed_info, load_yaml
+from .configs.plotting.read_results import get_seed_info, load_yaml, SINGLE_FIDELITY_ALGORITHMS
 from .configs.plotting.styles import X_LABEL, Y_LABEL
 from .configs.plotting.utils import (
     get_max_fidelity,
@@ -31,7 +31,14 @@ map_axs = (
 
 
 def _process_seed(
-    _path, seed, algorithm, key_to_extract, cost_as_runtime, results, n_workers
+    _path,
+    seed,
+    algorithm,
+    key_to_extract,
+    cost_as_runtime,
+    results,
+    n_workers,
+    parallel_sleep_decrement,
 ):
     print(
         f"[{time.strftime('%H:%M:%S', time.localtime())}] "
@@ -45,6 +52,7 @@ def _process_seed(
             algorithm=algorithm,
             cost_as_runtime=cost_as_runtime,
             n_workers=n_workers,
+            parallel_sleep_decrement=parallel_sleep_decrement,
         )
         incumbent = np.minimum.accumulate(losses)
         cost = [i[key_to_extract] for i in infos]
@@ -211,6 +219,7 @@ def plot(args):
                                 args.cost_as_runtime,
                                 results,
                                 args.n_workers,
+                                args.parallel_sleep_decrement,
                             )
                             for seed in seeds
                         )
@@ -227,6 +236,7 @@ def plot(args):
                             args.cost_as_runtime,
                             results,
                             args.n_workers,
+                            args.parallel_sleep_decrement,
                         )
                         for seed in seeds
                     ]
@@ -259,6 +269,7 @@ def plot(args):
                             args.cost_as_runtime,
                             results,
                             args.n_workers,
+                            args.parallel_sleep_decrement
                         )
                         for seed in seeds
                     )
@@ -275,13 +286,17 @@ def plot(args):
                         args.cost_as_runtime,
                         results,
                         args.n_workers,
+                        args.parallel_sleep_decrement
                     )
                     for seed in seeds
                 ]
 
             print(f"Time to process algorithm data: {time.time() - algorithm_starttime}")
 
-            ax = map_axs(axs, benchmark_idx, len(args.benchmarks), ncols)
+            # If you got an error, fix it here
+            #ax = map_axs(axs, benchmark_idx, len(args.benchmarks), ncols)
+            ax = axs.flatten()[benchmark_idx]
+            # ax = map_axs(axs, benchmark_idx, nrows, ncols)
             x = results["costs"][:]
             y = results["incumbents"][:]
             max_cost = None if args.cost_as_runtime else max(results["max_costs"][:])
@@ -295,8 +310,14 @@ def plot(args):
                 max_cost = get_max_fidelity(benchmark_name=benchmark)
 
             df = interpolate_time(
-                incumbents=y, costs=x, x_range=args.x_range, scale_x=max_cost
+                incumbents=y,
+                costs=x,
+                x_range=args.x_range,
+                scale_x=max_cost, 
+                rounded_integer_costs_for_x_range=algorithm in SINGLE_FIDELITY_ALGORITHMS,
+                parallel_evaluations=(args.n_workers > 1)
             )
+
 
             import pandas as pd
 
@@ -360,9 +381,11 @@ def plot(args):
 
     sns.despine(fig)
 
-    handles, labels = map_axs(
-        axs, 0, len(args.benchmarks), ncols
-    ).get_legend_handles_labels()
+
+    handles, labels = axs.flatten()[0].get_legend_handles_labels()
+    #handles, labels = map_axs(
+    #    axs, 0, len(args.benchmarks), ncols
+    #).get_legend_handles_labels()
 
     handles_to_plot, labels_to_plot = [], []
     handles_default, labels_default = [], []

src/mf_prior_experiments/plot_rank.py
@@ -11,7 +11,7 @@ from attrdict import AttrDict
 from joblib import Parallel, delayed, parallel_backend
 from scipy import stats
 
-from .configs.plotting.read_results import get_seed_info
+from .configs.plotting.read_results import get_seed_info, SINGLE_FIDELITY_ALGORITHMS
 from .configs.plotting.styles import ALGORITHMS, COLOR_MARKER_DICT, X_LABEL
 from .configs.plotting.utils import (
     get_max_fidelity,
@@ -33,7 +33,14 @@ experiment_group_titles = [
 
 
 def _process_seed(
-    _path, seed, algorithm, key_to_extract, cost_as_runtime, results, n_workers
+    _path,
+    seed,
+    algorithm,
+    key_to_extract,
+    cost_as_runtime,
+    results,
+    n_workers,
+    parallel_sleep_decrement,
 ):
     print(
         f"[{time.strftime('%H:%M:%S', time.localtime())}] "
@@ -47,7 +54,20 @@ def _process_seed(
             algorithm=algorithm,
             cost_as_runtime=cost_as_runtime,
             n_workers=n_workers,
+            parallel_sleep_decrement=parallel_sleep_decrement,
         )
+        if n_workers > 1:
+            # In parallel setup, we need to order them by cost, which
+            # is essentially the time at which they completed. This
+            # allow the incumbent traces to be correctly ordered to
+            # the x axis
+            losses, infos = zip(
+                *sorted(
+                    zip(losses, infos),
+                    key=lambda e: e[1][key_to_extract]
+                )
+            )
+
         incumbent = np.minimum.accumulate(losses)
         cost = [i[key_to_extract] for i in infos]
         results["incumbents"].append(incumbent)
@@ -136,6 +156,10 @@ def plot(args):
                 )
 
             for algorithm in args.algorithms:
+                print(
+                    f"[{time.strftime('%H:%M:%S', time.localtime())}] "
+                    f"[{benchmark_idx}] Processing algorithm {algorithm}..."
+                )
                 _path = os.path.join(_base_path, f"algorithm={algorithm}")
                 if not os.path.isdir(_path):
                     raise FileNotFoundError(
@@ -162,6 +186,7 @@ def plot(args):
                                 args.cost_as_runtime,
                                 results,
                                 args.n_workers,
+                                args.parallel_sleep_decrement,
                             )
                             for seed in seeds
                         )
@@ -178,6 +203,7 @@ def plot(args):
                             args.cost_as_runtime,
                             results,
                             args.n_workers,
+                            args.parallel_sleep_decrement,
                         )
                         for seed in seeds
                     ]
@@ -186,20 +212,20 @@ def plot(args):
                     f"Time to process algorithm data: {time.time() - algorithm_starttime}"
                 )
 
-                x = results["costs"][:]
-                y = results["incumbents"][:]
+                x = np.asarray(results["costs"][:])
+                y = np.asarray(results["incumbents"][:])
                 max_cost = None if args.cost_as_runtime else max(results["max_costs"][:])
-
-                if isinstance(x, list):
-                    x = np.array(x)
-                if isinstance(y, list):
-                    y = np.array(y)
 
                 if args.n_workers > 1 and max_cost is None:
                     max_cost = get_max_fidelity(benchmark_name=benchmark)
 
                 df = interpolate_time(
-                    incumbents=y, costs=x, x_range=args.x_range, scale_x=max_cost
+                    incumbents=y,
+                    costs=x,
+                    x_range=args.x_range,
+                    scale_x=max_cost,
+                    parallel_evaluations=(args.n_workers > 1),
+                    rounded_integer_costs_for_x_range=(algorithm in SINGLE_FIDELITY_ALGORITHMS)
                 )
 
                 import pandas as pd