be855e76e47580acaa6e7766c3979fd3050aac25
eddiebergman <eddiebergmanhs@gmail.com>
2023-02-19 13:46:10 +0100

    refactor: Cleanup dead files

--- src/mf_prior_experiments/configs/plotting/plot.py
+++ src/mf_prior_experiments/configs/plotting/plot.py
@@ -4,6 +4,7 @@ import math
 import sys
 import time
 from pathlib import Path
+from argparse import ArgumentParser, Namespace
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,7 +13,7 @@ import seaborn as sns
 from scipy import stats
 from typing_extensions import Literal
 
-from .configs.plotting.styles import (
+from .styles import (
     ALGORITHMS,
     BENCHMARK_COLORS,
     COLOR_MARKER_DICT,
@@ -21,9 +22,9 @@ from .configs.plotting.styles import (
     Y_LABEL,
     Y_LIMITS,
     get_xticks,
+    RC_PARAMS,
 )
-from .configs.plotting.types import ExperimentResults, fetch_results
-from .configs.plotting.utils import parse_args, set_general_plot_style
+from .types import ExperimentResults, fetch_results
 
 HERE = Path(__file__).parent.absolute()
 DEFAULT_BASE_PATH = HERE.parent.parent
@@ -417,7 +418,6 @@ def main():
     )
 
     plot_dir = base_path / "plots" / experiment_group
-    set_general_plot_style()
 
     xaxis = "cumulated_fidelity"
     yaxes = ["loss", "max_fidelity_loss"]
@@ -550,6 +550,67 @@ def main():
             fig.savefig(filepath, bbox_inches="tight", dpi=args.dpi)
             print(f"Saved to {filename} to {filepath}")
 
+
+def parse_args() -> Namespace:
+    plt.rcParams.update(RC_PARAMS)
+    parser = ArgumentParser(description="mf-prior-exp plotting")
+
+    parser.add_argument("--filename", type=str, default=None)
+    parser.add_argument("--collect", action="store_true")
+    parser.add_argument("--use-cache", action="store_true")
+
+    parser.add_argument("--experiment_group", type=str, required=True)
+    parser.add_argument("--algorithms", nargs="+", default=[])
+
+    parser.add_argument("--benchmarks", nargs="+", default=[])
+    parser.add_argument("--rr-good-corr-good-prior", nargs="+", default=[])
+    parser.add_argument("--rr-good-corr-bad-prior", nargs="+", default=[])
+    parser.add_argument("--rr-bad-corr-good-prior", nargs="+", default=[])
+    parser.add_argument("--rr-bad-corr-bad-prior", nargs="+", default=[])
+
+    parser.add_argument("--base_path", type=Path, default=None)
+    parser.add_argument("--n_workers", type=int, default=1)
+    parser.add_argument("--budget", nargs="+", type=float, default=None)
+    parser.add_argument("--x_range", nargs=2, type=float, default=None)
+    parser.add_argument("--x_together", type=float, default=None)
+
+    parser.add_argument("--dpi", type=int, default=200)
+    parser.add_argument("--ext", type=str, choices=["pdf", "png"], default="png")
+    parser.add_argument("--plot_default", action="store_true")
+    parser.add_argument("--plot_optimum", action="store_true")
+    #parser.add_argument("--dynamic_y_lim", action="store_true")
+    parser.add_argument("--parallel", action="store_true")
+
+    args = parser.parse_args()
+
+    if args.collect and args.use_cache:
+        raise ValueError("Can't use --collect with --use-cache")
+
+    if not args.collect and args.filename is None:
+        raise ValueError("Must specify --filename unless using --collect")
+
+    if args.x_together and args.x_range and args.x_together < args.x_range[0]:
+        raise ValueError("--x_together must be larger than --x_range[0]")
+
+    if not args.collect and len(args.algorithms) == 0:
+        raise ValueError("Must specify --algorithms unless using --collect")
+
+    if args.budget:
+        raise ValueError("CD plots (which use --budget) not supported yet")
+
+    benches = [
+        args.rr_good_corr_good_prior,
+        args.rr_good_corr_bad_prior,
+        args.rr_bad_corr_good_prior,
+        args.rr_bad_corr_bad_prior,
+    ]
+    if any(len(b) > 0 for b in benches) and not all(len(b) > 0 for b in benches):
+        raise ValueError("Must specify all --rr args for relative rankings")
+
+        #if not len(list(flatten(benches))) == len(set(flatten(benches))):
+        #raise ValueError("Benchmarks in --rr must all be unique\n")
+
+    return args
 
 if __name__ == "__main__":
     main()
